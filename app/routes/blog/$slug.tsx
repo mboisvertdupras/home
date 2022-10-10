97473548fd99805e78784b15cb05bf849514da7b
Marc Boisvert <marc@tolle.marketing>
2022-10-09 23:41:46 -0400

    Add meta information, tweak featured image position

--- app/routes/blog/$slug.tsx
+++ app/routes/blog/$slug.tsx
@@ -1,21 +1,16 @@
 import { useLoaderData } from "@remix-run/react";
+import type { MetaFunction } from "@remix-run/node";
 import { client } from "graphql/client";
 import { gql } from "@apollo/client";
 import dayjs from "dayjs";
-
-import PublicError from "~/components/PublicError";
 
 type Category = {
     name: string;
 }
 
-export function ErrorBoundary({ error }) {
-  return (
-    <div className="mx-auto max-w-3xl">
-      <PublicError error={error} />
-    </div>
-  );
-}
+export const meta: MetaFunction = ({ data }) => ({
+  title: `${data.data.post.title} | Marc Boisvert-Dupras `
+});
 
 export const loader = async ({ params }) => {
   const res = await client.query({
@@ -51,14 +46,6 @@ export default function Post() {
 
   return (
     <article>
-      {post.featuredImage && (
-        <img
-          className="mx-auto mb-12 w-full max-w-7xl"
-          src={post.featuredImage.node.sourceUrl}
-          srcSet={post.featuredImage.node.srcSet}
-          alt={post.featuredImage.node.caption}
-        />
-      )}
       <div className="mx-auto max-w-3xl">
         <div className="mb-2 text-xs uppercase tracking-wide text-sage-500">
           {dayjs(post.date).format("MMMM DD, YYYY")} /{" "}
@@ -70,9 +57,21 @@ export default function Post() {
             ))
             .reduce((prev, curr) => [prev, ", ", curr])}
         </div>
-        <h1 className="mt-3 mb-10 font-display text-4xl sm:text-7xl">
+        <h1 className="mt-3 font-display text-4xl sm:text-7xl">
           {post.title}
         </h1>
+      </div>
+      {post.featuredImage && (
+        <div className="relative mx-auto my-16 max-w-7xl">
+          <img
+            className="w-full"
+            src={post.featuredImage.node.sourceUrl}
+            srcSet={post.featuredImage.node.srcSet}
+            alt={post.featuredImage.node.caption}
+          />
+        </div>
+      )}
+      <div className="mx-auto max-w-3xl">
         <div
           className="prose text-lg"
           dangerouslySetInnerHTML={{ __html: post.content }}
